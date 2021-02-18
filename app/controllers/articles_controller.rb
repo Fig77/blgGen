35e70227a0bfaa1b754dafdd8096dfddfff726a7
fig77 <fig.igle@gmail.com>
2021-02-18 14:42:23 -0300

    Added session path and models railations back again, using node < 15.x.x since that causes excruciating loops rebuilding node_sass with yarn 1.2.x

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,46 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def cretae
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Article.destroy(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
